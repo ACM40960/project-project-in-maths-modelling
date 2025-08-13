81fe7868ff82c830b3e60ad889b5ba1381a8968a
Ubuntu2 <rye10140210@gmail.com>
2025-08-14 01:08:24 +0800

    Fix PicklingError by updating import

--- code/ultralytics_head/ultralytics/yolo/cfg/__init__.py
+++ code/ultralytics_head/ultralytics/yolo/cfg/__init__.py
@@ -10,8 +10,9 @@ from types import SimpleNamespace
 from typing import Dict, List, Union
 
 from ultralytics.yolo.utils import (DEFAULT_CFG, DEFAULT_CFG_DICT, DEFAULT_CFG_PATH, LOGGER, ROOT, USER_CONFIG_DIR,
-                                    IterableSimpleNamespace, __version__, checks, colorstr, deprecation_warn,
+                                    __version__, checks, colorstr, deprecation_warn,
                                     get_settings, yaml_load, yaml_print)
+from ultralytics.utils import IterableSimpleNamespace
 
 # Define valid tasks and modes
 MODES = 'train', 'val', 'predict', 'export', 'track', 'benchmark'
